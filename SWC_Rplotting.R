be21c2e71806b6d98365e4dee4e9dce9bd8aa35b
chdrudde <chdrudde.derudder@ugent.be>
2017-09-15 15:39:43 +0200

    Added FM's part on data wrangling with dplyr

--- SWC_Rplotting.R
+++ SWC_Rplotting.R
@@ -1,4 +1,11 @@
-# R script SWC part Ruben R
+# R script SWC part Ruben R on plotting and part FM on dplyr data wrangling
+
+##### Loading libraries ####
+library(ggplot2)
+library(dplyr)
+library(gapminder)
+
+#### Reading the data ####
 
 df<-read.csv("Metadata.csv",nrows=77)
 View(df)
@@ -9,7 +16,6 @@ View(df)
 str(df)
 
 ##### Start plotting ####
-library(ggplot2)
 
 #Make first plot
 
@@ -41,7 +47,7 @@ p4<-p4 + (aes(fill=Reactor.phase,color=Reactor.phase))
 
 #Meanwhile, git again: save the file, open git in tab, click stage
 
-# Challenge time
+##### Challenge time ####
 ## Right side: Conductivity (us)
 ## Middle: Diversity
 ## Left: Cell density
@@ -51,9 +57,39 @@ p5<-ggplot(data=df, aes(x=Timepoint, y=Conductivity))+
   facet_grid(~Reactor.cycle)+
   geom_line(lty=2,colour="black")+
   geom_point(size=3, alpha=0.5, aes(fill=Reactor.phase,color=Reactor.phase))+
-  geom_line(aes(color=Reactor.phase))+
+  geom_line(aes(color=Reactor.phase),size=1)+
   theme_bw()+
   ggtitle("Another awesome plot")
 p5
 
+#### Data frame manipulation with dplyr ####
+
+
+#### load data ####
+df<-read.csv("Metadata.csv")
+
+#### calculate the mean of  ####
+
+mean(df[df$Reactor.phase=="Control","ph"])
+levels(df$Reactor.phase)
+
+##### Select, filter and pipe #####
+physicochem <- dplyr::select(df,ph,temp, Conductivity)
+
+# R has an equivalent of piping %>% (Ctrl + Shift + M is shortcut for this) aaaaaaannnd you can use this with tab completion
+# so no more spelling mistakes and pipes can be combined
+
+physicochem <- df %>% select(ph,temp,Conductivity)
+
+physicochem.control <- df %>% 
+  filter(Reactor.phase == "Control") %>% 
+  select(ph,temp,Conductivity)
+
+# Challenge: select diversity parameters for reactor phase startup
+diversity.startup <- df %>% 
+  filter(Reactor.phase == "Startup") %>% 
+  #select(Diversity...D0,Diversity...D1,Diversity...D2)
+  select(contains("Diversity"))
+
+#### group_by and summarize ####
 
